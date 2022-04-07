97fd055f68c4012118aa1f832b811e874b9ecda3
fullyonline <no.reply4me@gmail.com>
2022-04-07 20:34:25 +0200

    added Rental

---
+++ src/main/java/ch/juventus/carrental/model/Rental.java
@@ -0,0 +1,9 @@
+package ch.juventus.carrental.model;
+
+import java.util.Date;
+
+public class Rental {
+    Date startDate;
+    Date endDate;
+    Double totalPrice; // anzahl Tage * PreisProTag
+}
