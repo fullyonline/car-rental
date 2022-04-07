e04e7e1fef36cc942f7c457f6f373478db6c5e00
fullyonline <no.reply4me@gmail.com>
2022-04-07 20:34:56 +0200

    added CarDatabase with dummy stuff

---
+++ src/main/java/ch/juventus/carrental/persistance/CarDatabase.java
@@ -0,0 +1,15 @@
+package ch.juventus.carrental.persistance;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CarDatabase {
+
+    // read / write data
+    // soll interface geben
+
+    public String loadGreeting(){
+        return "Hello World";
+    }
+
+}
