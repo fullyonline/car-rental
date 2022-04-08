a9511ff5a8758177f8bceef1fe22a89a14463edf
fullyonline <no.reply4me@gmail.com>
2022-04-08 11:14:44 +0200

    added: getter and setter to Car

--- src/main/java/ch/juventus/carrental/model/Car.java
+++ src/main/java/ch/juventus/carrental/model/Car.java
@@ -9,6 +9,71 @@ public class Car {
     private Enum<GearShift> gearShift;
     private Integer seats;
     private Double pricePerDay;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Enum<CarType> getType() {
+        return type;
+    }
+
+    public void setType(Enum<CarType> type) {
+        this.type = type;
+    }
+
+    public Enum<GearShift> getGearShift() {
+        return gearShift;
+    }
+
+    public void setGearShift(Enum<GearShift> gearShift) {
+        this.gearShift = gearShift;
+    }
+
+    public Integer getSeats() {
+        return seats;
+    }
+
+    public void setSeats(Integer seats) {
+        this.seats = seats;
+    }
+
+    public Double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(Double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public Boolean getAirCondition() {
+        return airCondition;
+    }
+
+    public void setAirCondition(Boolean airCondition) {
+        this.airCondition = airCondition;
+    }
+
+    public List<Rental> getRentaly() {
+        return rentaly;
+    }
+
+    public void setRentaly(List<Rental> rentaly) {
+        this.rentaly = rentaly;
+    }
+
     private Boolean airCondition;
     private List<Rental> rentaly; 	// kann als array abgespeichert werden.
 }
