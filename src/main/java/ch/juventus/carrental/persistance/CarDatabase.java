7117b526f12af3eef660a122719d40e9a2030dd0
fullyonline <no.reply4me@gmail.com>
2022-04-08 11:14:20 +0200

    added: funtcion definitions to CarDatabase

--- src/main/java/ch/juventus/carrental/persistance/CarDatabase.java
+++ src/main/java/ch/juventus/carrental/persistance/CarDatabase.java
@@ -1,4 +1,13 @@
 package ch.juventus.carrental.persistance;
 
+import ch.juventus.carrental.model.Car;
+
 public interface CarDatabase {
+
+    Long getNewId();
+    void create(Car car);
+    void delete(Integer id);
+    void update(Car car);
+    void select(Integer id);
+    void selectAll();
 }
--- src/main/java/ch/juventus/carrental/persistance/FileCarDatabase.java
+++ src/main/java/ch/juventus/carrental/persistance/FileCarDatabase.java
@@ -1,11 +1,43 @@
 package ch.juventus.carrental.persistance;
 
+import ch.juventus.carrental.model.Car;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
+
+import java.nio.file.Paths;
+import java.util.List;
 
 @Repository
 public class FileCarDatabase implements CarDatabase {
 
     // read / write data
-    // soll interface geben
 
+    @Override
+    public Long getNewId() {
+        return Long.valueOf(1); // TODO: fix me
+    }
+
+    @Override
+    public void create(Car car) {
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+
+    @Override
+    public void update(Car car) {
+
+    }
+
+    @Override
+    public void select(Integer id) {
+
+    }
+
+    @Override
+    public void selectAll() {
+
+    }
 }
