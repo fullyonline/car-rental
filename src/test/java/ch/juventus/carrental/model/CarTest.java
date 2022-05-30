2018c0efe53707879e9681bfc5956b67af64bc95
fullyonline <no.reply4me@gmail.com>
2022-05-30 19:22:33 +0200

    added test for the Car class

---
+++ src/test/java/ch/juventus/carrental/model/CarTest.java
@@ -0,0 +1,34 @@
+package ch.juventus.carrental.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void carIsValid() {
+        Car car = new Car(1L, "BMW 3", CarType.CABRIO, GearShift.MANUAL, 2, 1000d, false, new ArrayList<>());
+        assertTrue(car.isValid());
+    }
+
+    @Test
+    void carIsNotValid() {
+        Car car = new Car(null, null, null, null, null, null, null, null);
+        assertFalse(car.isValid());
+    }
+
+    @Test
+    void notValidCarNegativeSeats() {
+        Car car = new Car(1L, "BMW 3", CarType.CABRIO, GearShift.MANUAL, -2, 1000d, false, new ArrayList<>());
+        assertFalse(car.isValid());
+    }
+
+    @Test
+    void notValidCarNegativePricePerDay() {
+        Car car = new Car(1L, "BMW 3", CarType.CABRIO, GearShift.MANUAL, 2, -1d, false, new ArrayList<>());
+        assertFalse(car.isValid());
+    }
+}
