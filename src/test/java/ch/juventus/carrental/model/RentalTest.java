170bb7ce59ff7462e4ed3f149b088b5e1ba6a3d6
fullyonline <no.reply4me@gmail.com>
2022-05-30 20:58:14 +0200

    added testing if the rental is valid

---
+++ src/test/java/ch/juventus/carrental/model/RentalTest.java
@@ -0,0 +1,69 @@
+package ch.juventus.carrental.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RentalTest {
+
+    private SimpleDateFormat sdf;
+    private Rental rental;
+
+    @BeforeEach
+    void beforeEach(){
+        rental = new Rental();
+        sdf = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMAN);
+    }
+
+    @Test
+    void rentalIsValid() {
+        try{
+            rental.setStartDate(sdf.parse("29.05.2022"));
+            rental.setEndDate(sdf.parse("30.05.2022"));
+
+        } catch (ParseException e) {
+            // This is a unit test. this works :)
+        }
+        assertTrue(rental.isValid());
+    }
+
+    @Test
+    void rentalIsInvalid() {
+        try{
+            rental.setStartDate(sdf.parse("30.05.2022"));
+            rental.setEndDate(sdf.parse("29.05.2022"));
+
+        } catch (ParseException e) {
+            // This is a unit test. this works :)
+        }
+        assertFalse(rental.isValid());
+    }
+
+    @Test
+    void rentalIsMissingEndDate() {
+        try{
+            rental.setEndDate(sdf.parse("29.05.2022"));
+
+        } catch (ParseException e) {
+            // This is a unit test. this works :)
+        }
+        assertFalse(rental.isValid());
+    }
+
+    @Test
+    void rentalIsMissingStartDate() {
+        try{
+            rental.setStartDate(sdf.parse("29.05.2022"));
+
+        } catch (ParseException e) {
+            // This is a unit test. this works :)
+        }
+        assertFalse(rental.isValid());
+    }
+}
