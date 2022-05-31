c9d837ae17915a83835778d5cc78bbe319bc4b22
Roman Inglin <roman.inglin@gmail.com>
2022-05-31 20:29:31 +0200

    Refactored DateValidator

--- src/main/java/ch/juventus/carrental/service/DateValidator.java
+++ src/main/java/ch/juventus/carrental/service/DateValidator.java
@@ -6,8 +6,12 @@ public class DateValidator {
     public static boolean validate(Date startDate, Date endDate) {
         if (startDate != null && endDate != null)
         {
-            return endDate.compareTo(startDate) >= 0;
+            return isValidRange(startDate, endDate);
         }
         return false;
+    }
+
+    private static boolean isValidRange(Date startDate, Date endDate) {
+        return endDate.compareTo(startDate) >= 0;
     }
 }
