b615944a82d402efe784e81ceba7cb24ae2736c2
fullyonline <no.reply4me@gmail.com>
2022-05-31 20:31:02 +0200

    added comments to the CarDatabase interface

--- src/main/java/ch/juventus/carrental/persistance/CarDatabase.java
+++ src/main/java/ch/juventus/carrental/persistance/CarDatabase.java
@@ -7,10 +7,46 @@ import java.util.Map;
 
 public interface CarDatabase {
 
+    /**
+     *
+     * @param car a new Car class to add to the database
+     * @return if the insert was successful
+     */
     Boolean create(Car car);
+
+    /**
+     *
+     * @param id of the car to delete
+     * @return if the delete was successful
+     */
     Boolean delete(Long id);
+
+    /**
+     *
+     * @param id of the car to update
+     * @param car the updated car object
+     * @return if the update was successful
+     */
     Boolean update(Long id, Car car);
+
+    /**
+     *
+     * @param id of the car class which is to select
+     * @return the searched car
+     */
     Car select(Long id);
+
+    /**
+     *
+     * @return all saved cars
+     */
     Map<Long, Car> select();
+
+    /**
+     *
+     * @param carId the id of the car
+     * @param rental the new rental of the car
+     * @return if the creation of the new rental was successful
+     */
     Boolean createRental(Long carId, Rental rental);
 }
