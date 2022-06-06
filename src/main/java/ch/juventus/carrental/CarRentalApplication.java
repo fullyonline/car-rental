2be8db40759c66fbfd82e076543c6ee5788337f1
Roman Inglin <roman.inglin@gmail.com>
2022-06-06 16:46:37 +0200

    Added API info for Swagger

--- src/main/java/ch/juventus/carrental/CarRentalApplication.java
+++ src/main/java/ch/juventus/carrental/CarRentalApplication.java
@@ -1,7 +1,10 @@
 package ch.juventus.carrental;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CarRentalApplication {
@@ -10,4 +13,16 @@ public class CarRentalApplication {
 		SpringApplication.run(CarRentalApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI openApiConfig() {
+		return new OpenAPI().info(createApiInfo());
+	}
+
+	private Info createApiInfo() {
+		Info info = new Info();
+		info.title("Car rental API")
+				.description("Car rental API as backend of Car rental application")
+				.version("v1.0.0");
+		return info;
+	}
 }
